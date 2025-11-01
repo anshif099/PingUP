a3fa949b8a2a17142452fc049d7b82031bb16caf
Anahif099 <rtsccp@gmail.com>
2025-11-01 21:13:41 +0530

    feat: Update PingUP logo to use local image
    - Changed PingUPLogo component to use /PingUP.jpg from public folder
    - Replaced external URL with local image path
    - Logo now loads from public/PingUP.jpg instead of external source

--- src/components/PingUPLogo.tsx
+++ src/components/PingUPLogo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PingUPLogo: React.FC<{ className?: string }> = ({ className = "w-8 h-8" }) => (
   <img
-    src="https://mma.prnewswire.com/media/342968/pingup_logo_vertical_Logo.jpg?p=publish&w=950"
+    src="/PingUP.jpg"
     alt="PingUP Logo"
     className={className}
   />
